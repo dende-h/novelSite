8d6917505b070640528c91b89d9855442b721379
dende-h <miyuki_dende@yahoo.co.jp>
2021-08-11 16:20:02 +0900

    クラス名変更

--- novelSite/src/main/java/io/post/novel/controller/UserSignUpController.java
+++ novelSite/src/main/java/io/post/novel/controller/UserSignUpController.java
@@ -2,12 +2,15 @@ package io.post.novel.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 import io.post.novel.service.NovelService;
 
 @Controller
-public class NovelController {
+public class UserSignUpController {
 	
 	@Autowired
 	NovelService novelService;
@@ -29,8 +32,13 @@ public class NovelController {
 		return "sign_up_form";
 	}
 	
-	
-	
+	/*
+	 * @return 入力確認
+	 */
+	@RequestMapping(value = "/input/check", method = RequestMethod.POST)
+	public String inputCheckDisplay(Model model) {
+		return "input_check";
+	}
 	
 	
 	
