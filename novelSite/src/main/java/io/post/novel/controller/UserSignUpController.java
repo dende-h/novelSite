02098beb74d34159eeea1d19d72f34fba711a6dd
dende-h <miyuki_dende@yahoo.co.jp>
2021-08-15 15:50:58 +0900

    エラーメッセージの表示場所変更

--- novelSite/src/main/java/io/post/novel/controller/UserSignUpController.java
+++ novelSite/src/main/java/io/post/novel/controller/UserSignUpController.java
@@ -1,13 +1,9 @@
 package io.post.novel.controller;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
-import org.springframework.validation.ObjectError;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
@@ -53,11 +49,11 @@ public class UserSignUpController {
 				+ userRequest.getBirthDay() + "日"
 				+ userRequest.getUserCategory());
 		  if (result.hasErrors()) {
-	            List<String> errorList = new ArrayList<String>();
+	           /* List<String> errorList = new ArrayList<String>();
 	            for (ObjectError error : result.getAllErrors()) {
 	                errorList.add(error.getDefaultMessage());
 	            }
-	            model.addAttribute("validationError", errorList);
+	            model.addAttribute("validationError", errorList);*/
 	            return "sign_up_form";
 	        }
 		
